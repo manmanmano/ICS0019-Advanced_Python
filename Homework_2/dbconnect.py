d91c22db3f54ebff59cfd09dac3e26cdc36c0656
manmanmano <jimiskools@gmail.com>
2022-04-19 14:00:39 +0200

    added providers

--- Homework_2/dbconnect.py
+++ Homework_2/dbconnect.py
@@ -14,7 +14,7 @@ def create_tables():
     """
     Create tables CANTEEN and PROVIDER in DINERS
     """
-    conn.execute('''CREATE TABLE CANTEEN
+    conn.execute('''CREATE TABLE IF NOT EXISTS CANTEEN
             (ID INTEGER PRIMARY KEY AUTOINCREMENT,
             ProviderID TEXT NOT NULL, 
             Name TEXT NOT NULL,
@@ -23,11 +23,26 @@ def create_tables():
             time_closed TIME NOT NULL,
             FOREIGN KEY(ProviderID) REFERENCES PROVIDER(ProviderName));''')
 
-    conn.execute('''CREATE TABLE PROVIDER
+    conn.execute('''CREATE TABLE IF NOT EXISTS PROVIDER
             (ID INTEGER PRIMARY KEY AUTOINCREMENT,
             ProviderName TEXT NOT NULL);''')
 
     print("Tables created successfully!")
+
+
+def createRecords():
+    """
+    Create required records in tables CANTEEN and PROVIDER
+    """
+    cursor = conn.cursor()
+    provider_data = ["Rahva toit", "Balitc Restaurants Estonia AS", "TTÜ Sport OÜ", "bitStop Kohvik OÜ"]
+    for data in provider_data: 
+        format_str = """INSERT INTO PROVIDER (ProviderName) VALUES ("{provider_name}");"""
+        sql_command = format_str.format(provider_name=data)
+        cursor.execute(sql_command)
+
+    conn.commit()
+    print("Data records inserted successfully!")
 
 
 def closeconn():
@@ -41,3 +56,5 @@ def closeconn():
 if __name__ == "__main__":
     opendb()
     create_tables()
+    createRecords()
+    closeconn()
