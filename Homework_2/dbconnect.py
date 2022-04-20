655292f7d7e559541ed249d933b21b7693d191c9
manmanmano <jimiskools@gmail.com>
2022-04-20 15:09:33 +0200

    changed fltring

--- Homework_2/dbconnect.py
+++ Homework_2/dbconnect.py
@@ -22,26 +22,16 @@ def create_tables():
             Location VARCHAR(100) NOT NULL,
             time_open TIME NOT NULL,
             time_closed TIME NOT NULL,
-            FOREIGN KEY(ProviderID) REFERENCES PROVIDER(ID));''')
+            FOREIGN KEY(ProviderID) REFERENCES PROVIDER(ID),
+            UNIQUE(Name));''')
 
     # create table PROVIDER
     conn.execute('''CREATE TABLE IF NOT EXISTS PROVIDER
             (ID INTEGER PRIMARY KEY AUTOINCREMENT,
-            ProviderName TEXT NOT NULL);''')
+            ProviderName TEXT NOT NULL,
+            UNIQUE(ProviderName));''')
 
     print("Tables created successfully!")
-
-
-def checkRecords(cursor, table, column, data): 
-    """
-    Checks if value in column already exists. Returns True if record is not found.
-    """
-    cursor.execute(f"SELECT * FROM {table} WHERE {column} = ?", (data, ))
-    # get number of records
-    nr_records = cursor.fetchall()
-    # if record is not found execute, else do nothing
-    if len(nr_records) == 0:
-        return True
 
 
 def createRecords():
@@ -52,36 +42,47 @@ def createRecords():
     cursor = conn.cursor()
     provider_data = ["Rahva toit", "Balitc Restaurants Estonia AS", "TTÜ Sport OÜ", "bitStop Kohvik OÜ"]
     for data in provider_data: 
-        format_str = """INSERT INTO PROVIDER (ProviderName) VALUES ("{provider_name}");"""
+        format_str = """INSERT OR IGNORE INTO PROVIDER (ProviderName) VALUES ("{provider_name}");"""
         sql_command = format_str.format(provider_name=data)
-        if checkRecords(cursor, "PROVIDER", "ProviderName",  data):
-            cursor.execute(sql_command)
-
-    # add IT-College data with a separate statement as requirement said
-    sql_command = """INSERT INTO CANTEEN (ProviderID, Name, Location, time_open, time_closed)
-    VALUES ("4", "bitStop KOHVIK", "IT College, Raja 4c", "9:30", "16:00")"""
-    if checkRecords(conn.cursor(), "CANTEEN", "Location", "IT College, Raja 4c"):
         cursor.execute(sql_command)
 
+    # add IT-College data with a separate statement as requirement said
+    sql_command = """INSERT OR IGNORE INTO CANTEEN (ProviderID, Name, Location, time_open, time_closed)
+    VALUES ("4", "bitStop KOHVIK", "IT College, Raja 4c", "930", "1600")"""
+    cursor.execute(sql_command)
+
     # add remaining canteen data with lists
-    canteen_data = [("1", "Economics- and social science building canteen", "Akadeemia tee 3, SOC-building", "8:30", "18:30"), 
-        ("1", "Library canteen", "Akadeemia tee 1/Ehitajate tee 7", "8:30", "19:00"), 
-        ("2", "Main building Deli cafe", "Ehitajate tee 5, U01 building", "9:00", "16:30"),
-        ("2", "Main building Daily lunch restaurant", "Ehitajate tee 5, U01 building", "9:00", "16:30"),
-        ("1", "U06 building canteen", "U06 building", "9:00", "16:00"),
-        ("2", "Natural Science building canteen", "Akadeemia tee 15, SCI building", "9:00", "16:00"),
-        ("2", "ICT building canteen", "Raja 15/Mäepealse 1", "9:00", "16:00"),
-        ("3", "Sports building canteen", "Männiliiva 7, S01 building", "11:00", "20:00")] 
+    canteen_data = [("1", "Economics- and social science building canteen", "Akadeemia tee 3, SOC-building", "830", "1830"), 
+        ("1", "Library canteen", "Akadeemia tee 1/Ehitajate tee 7", "830", "1900"), 
+        ("2", "Main building Deli cafe", "Ehitajate tee 5, U01 building", "900", "1630"),
+        ("2", "Main building Daily lunch restaurant", "Ehitajate tee 5, U01 building", "1400", "1630"),
+        ("1", "U06 building canteen", "U06 building", "900", "1600"),
+        ("2", "Natural Science building canteen", "Akadeemia tee 15, SCI building", "900", "1600"),
+        ("2", "ICT building canteen", "Raja 15/Mäepealse 1", "900", "1600"),
+        ("3", "Sports building canteen", "Männiliiva 7, S01 building", "1100", "2000")] 
     for data in canteen_data: 
-        format_str = """INSERT INTO CANTEEN (ProviderID, Name, Location, time_open, time_closed) 
+        format_str = """INSERT OR IGNORE INTO CANTEEN (ProviderID, Name, Location, time_open, time_closed) 
         VALUES ("{provider_id}", "{name}", "{location}", "{time_open}", "{time_closed}");"""
         sql_command = format_str.format(provider_id=data[0], name=data[1], location=data[2], time_open=data[3], time_closed=data[4])
-        if checkRecords(cursor, "CANTEEN", "Location",  data[2]):
-            cursor.execute(sql_command)
+        cursor.execute(sql_command)
 
 
     conn.commit()
-    print("Data records inserted successfully!")
+    print("Data records inserted successfully!\n")
+
+
+def selectRecords():
+    """
+    fetch and display records from the CANTEEN table
+    """
+    cursor = conn.execute("SELECT * FROM CANTEEN WHERE time_open <= 1615 AND time_closed >= 1800")
+    for row in cursor:
+        print("ID = ", row[0])
+        print("PROVIDER ID = ", row[1])
+        print("NAME = ", row[2])
+        print("LOCATION = ", row[3])
+        print("OPENING TIME = ", row[4])
+        print("CLOSING TIME = ", row[5], '\n')
 
 
 def closeconn():
@@ -96,4 +97,5 @@ if __name__ == "__main__":
     opendb()
     create_tables()
     createRecords()
+    selectRecords()
     closeconn()
