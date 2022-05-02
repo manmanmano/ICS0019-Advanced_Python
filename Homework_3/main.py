83c5c5638f88c84a3c3045d057a49ee85c7c4056
manmanmano <jimiskools@gmail.com>
2022-05-03 01:58:06 +0300

    done

--- Homework_3/main.py
+++ Homework_3/main.py
@@ -4,6 +4,29 @@ import cartopy.feature as cf
 from matplotlib import pyplot as plt
 
 
+def createmap_Europe():
+    """
+    Draw a map of Europe using cartopy
+    """
+    proj = ccrs.PlateCarree()
+    ax = plt.axes(projection=proj)
+    ax.set_extent([-13, 45, 30, 70])
+    ax.stock_img()
+    ax.add_feature(cf.COASTLINE, lw=2)
+    ax.add_feature(cf.BORDERS)
+    plt.gcf().set_size_inches(20, 10)
+
+
+def draw_flights(flights_data, lat, lon, clr, mrkr):
+    """
+    Draw flights with desired color and style. Flights file must contain 
+    columns Latitude and Longitude.
+    """
+    # without index gives error because of tuple
+    for index, row in flights_data.iterrows():
+        plt.plot([lon, row['Longitude']], [lat, row['Latitude']], color=clr, marker=mrkr, transform=ccrs.Geodetic())
+
+
 # Read csv files, first is precovid flights, second postcovid flights, and last is airports
 precf = pd.read_csv("Files/precovid_flights.csv", sep=";")
 postcf = pd.read_csv("Files/postcovid_flights.csv", sep=";")
@@ -13,14 +36,16 @@ airports = pd.read_csv("Files/airports.dat", sep=",")
 precf_coord = pd.merge(precf, airports[['Longitude', 'Latitude', 'IATA']], on='IATA')
 postcf_coord = pd.merge(postcf, airports[['Longitude', 'Latitude', 'IATA']], on='IATA')
 
-# Draw a map of Europe using cartopy
-proj = ccrs.PlateCarree()
-ax = plt.axes(projection=proj)
-ax.set_extent([-13, 45, 30, 70])
-ax.stock_img()
-ax.add_feature(cf.COASTLINE, lw=2)
-ax.add_feature(cf.BORDERS)
-plt.gcf().set_size_inches(20, 10)
+# Draw the map of Europe
+createmap_Europe()
+
+# Create two variables holding Tallin's latitude and longitude
+tallinn_lat = 59.4162
+tallinn_lon = 24.8004
+
+# Call function in order to draw flight data, precovid will be in yellow, postcovid in blue
+draw_flights(precf_coord, tallinn_lat, tallinn_lon, 'yellow', 'o')
+draw_flights(postcf_coord, tallinn_lat, tallinn_lon, 'blue', 'o')
 
 # Save figure as PNG and set title
 plt.title("Pre- and Postcovid direct flights from Tallinn\nMariano D'Angelo\n", fontsize=20)
