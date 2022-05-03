e40dee54e698bd28a43732d1c6fb304901bc290d
manmanmano <jimiskools@gmail.com>
2022-05-03 18:02:41 +0300

    done

--- Homework_4/Soup/soup.py
+++ Homework_4/Soup/soup.py
@@ -1,5 +1,14 @@
+"""
+In order to get json format this script must be run as follows:
+
+python3 soup.py > soup_output.json !!! UNCOMMENT REQUIRED LINES 11, 31, 43, 45
+
+soup_output.json will contain the list of items.
+"""
+
 from bs4 import BeautifulSoup
 import requests
+# import json
 
 # url to search in
 start_url = 'https://ordi.eu/sulearvutid'
@@ -10,15 +19,27 @@ def parse(start_urls):
 
     # insert products into list
     chipsets_list = soup.find_all('li', class_ = 'item')
-     
+    
     for chip in chipsets_list:
-        data = {'Title':'', 'Price':'', 'Picture href':'',}
-        data['Title'] = chip.h2.get_text()
-        data['Price'] = chip.span.get_text()
-        data['Picture href'] = chip.img['src']
-        print(data)
+        data = {"Title":"", "Price":"", "Picture href":"",}
+        data["Title"] = chip.h2.get_text()
+        data["Price"] = chip.span.get_text()
+        data["Picture href"] = chip.img['src']
+        # needed to format data to json
+        # on output is needed to run certain command
+        # last comma in json must be manually deleted
+        # print(json.dumps(data) + ",")
 
     # go to the next page
+    try:
+        next_page = soup.find('a', class_ = 'next')['href']
+        if next_page:
+            parse(next_page)
+    except:
+        return
+
 
 if __name__ == '__main__':
+    # print("[")
     parse(start_url)
+    # print("]")
