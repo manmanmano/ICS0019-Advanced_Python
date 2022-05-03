ffe1638778bed3b316b7b1170fcb467990522ae4
manmanmano <jimiskools@gmail.com>
2022-05-03 13:39:10 +0300

    next page working

--- Homework_4/scraper.py
+++ Homework_4/scraper.py
@@ -1,7 +1,6 @@
 """
 Creating a Scraper that grabs from computer page title, price, and picture.
 Can be run with: scrapy runspider -o scraper.json scraper.py
-
 """
 
 import scrapy
@@ -21,10 +20,15 @@ class OrdiSetLaptops(scrapy.Spider):
 
 
     def parse(self, response):
+        """
+        In order to crawl and scrape through the page, we use css selectors, as
+        it is an easier option. Selectors are three, taken from the list selectors.
+        I_SELECTOR is the selector taken from the list each loop, changes every round.
+        """
         selectors = ['.item first', '.item', '.item last']
         for selector in selectors:
-            SET_SELECTOR = selector
-            for chipset in response.css(SET_SELECTOR):
+            I_SELECTOR = selector
+            for chipset in response.css(I_SELECTOR):
                 TITLE_SELECTOR = 'a::text'
                 PRICE_SELECTOR = 'span::text'
                 IMAGE_SELECTOR = 'img::attr(src)'
@@ -33,3 +37,10 @@ class OrdiSetLaptops(scrapy.Spider):
                         'price': chipset.css(PRICE_SELECTOR).extract_first(),
                         'image': chipset.css(IMAGE_SELECTOR).extract_first(),
                 }
+            # define a selector for the next page link
+            NEXT_PAGE_SELECTOR = '.next::attr(href)'
+            # extract the first match and check existance
+            next_page = response.css(NEXT_PAGE_SELECTOR).extract_first()
+            if next_page:
+                url = response.urljoin(next_page)
+                yield scrapy.Request(url, self.parse, headers=self.headers)
